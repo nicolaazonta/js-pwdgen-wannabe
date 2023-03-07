46cd22ebf8be39e809abfed84fca13c376b7d9ce
nicolaazonta <nicolazonta114@gmail.com>
2023-03-07 15:05:41 +0100

    write main points to do on main.js

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+//chiedere all'utente il suo nome
+
+//chiedere all'utente il suo cognome
+
+//chiedere il suo colore preferito
+
+//chiedere il suo numero preferito
+
+//scrivere il risultato usando il seguente formato nomecognomecolorepreferito23 preceduto da un testo a scelta
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
